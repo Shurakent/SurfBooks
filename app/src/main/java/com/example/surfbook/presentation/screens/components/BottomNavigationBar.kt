cfaab6d097a23f46db6ecd7b3d683d8b70bb998d
Shurakent <ghedc12@gmail.com>
2025-04-02 14:06:59 +0300

    bottom navigation ui new

--- app/src/main/java/com/example/surfbook/presentation/screens/components/BottomNavigationBar.kt
+++ app/src/main/java/com/example/surfbook/presentation/screens/components/BottomNavigationBar.kt
@@ -1,12 +1,12 @@
 package com.example.surfbook.presentation.screens.components
 
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -18,7 +18,6 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.surfbook.R
-import com.example.surfbook.presentation.navigation.Route
 
 
 @Composable
@@ -27,68 +26,66 @@ fun BottomNavigationBar(navController: NavHostController) {
     val currentRoute = navBackStackEntry?.destination?.route
     val colors = MaterialTheme.colorScheme
     val typo = MaterialTheme.typography
-    BottomNavigation(
-        backgroundColor = colors.background,
+
+    NavigationBar(
+        containerColor = colors.background,
         contentColor = colors.primary,
         modifier = Modifier.border(width = 1.dp, color = colors.secondary)
     ) {
-
-        BottomNavigationItem(
-            modifier = Modifier.padding(10.dp),
-            label = {Text(
-                "Поиск",
-                style = typo.labelMedium,
-                color = if (currentRoute == Route.SearchScreen().route) colors.primary else colors.secondary
-            )},
-            selected = currentRoute == Route.SearchScreen().route,
-            icon = {
-                androidx.compose.material3.Icon(
-                    painter = painterResource(id = R.drawable.ic_search_default), // Укажите ваш ресурс
-                    contentDescription = "Поиск",
-                    modifier = Modifier.size(size = 24.dp),
-                    tint = if (currentRoute == Route.SearchScreen().route) colors.primary else colors.secondary
-                )
-            },
-            onClick = {
-                navController.navigate(Route.SearchScreen().route) {
-                    popUpTo(navController.graph.findStartDestination().id) {
-                        saveState = true
+        NavBarItems.BarItems.forEach {navItem ->
+            NavigationBarItem(
+                selected = currentRoute == navItem.route,
+                onClick = {
+                    navController.navigate(navItem.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {saveState = true}
+                        launchSingleTop = true
+                        restoreState = true
                     }
-                    launchSingleTop = true
-                    restoreState = true
-                }
-            }
-        )
-        BottomNavigationItem(
-            modifier = Modifier.padding(10.dp),
-            icon = {
-                androidx.compose.material3.Icon(
-                    painter = painterResource(id = R.drawable.ic_favorite_default), // Укажите ваш ресурс
-                    contentDescription = "Избранное",
-                    modifier = Modifier.size(size = 24.dp),
-                    tint = if (currentRoute == Route.FavoritesScreen().route) colors.primary else colors.secondary
-                )
-            },
-            label = {
-                Text(
-                    "Избранное",
+                },
+                icon = {
+                    androidx.compose.material3.Icon(
+                        painter = painterResource(id = navItem.image),
+                        contentDescription = "Поиск",
+                        modifier = Modifier.size(size = 24.dp),
+                    )
+                },
+                label = {Text(
+                    navItem.title,
                     style = typo.labelMedium,
-                    color = if (currentRoute == Route.FavoritesScreen().route) colors.primary else colors.secondary
                 )},
-            selected = currentRoute == Route.FavoritesScreen().route,
-            onClick = {
-                navController.navigate(Route.FavoritesScreen().route) {
-                    popUpTo(navController.graph.findStartDestination().id) {
-                        saveState = true
-                    }
-                    launchSingleTop = true
-                    restoreState = true
-                }
-            }
-        )
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = colors.primary,
+                    selectedTextColor = colors.primary,
+                    unselectedIconColor = colors.secondary,
+                    unselectedTextColor = colors.secondary,
+                    indicatorColor = colors.background
+                )
+            )
+        }
     }
 }
-@Preview
+
+object NavBarItems {
+    val BarItems = listOf(
+        BarItem(
+            title = "Поиск",
+            image = R.drawable.ic_search_default,
+            route = "search"
+        ),
+        BarItem(
+            title = "Избранное",
+            image = R.drawable.ic_favorite_default,
+            route = "favorites"
+        )
+    )
+}
+
+data class BarItem(
+    val title: String,
+    val image: Int,
+    val route: String
+)
+@Preview(apiLevel=34)
 @Composable
 fun BottomNavigationBarPreview() {
     BottomNavigationBar(navController = rememberNavController())
